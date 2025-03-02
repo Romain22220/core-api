440e432f247794e1656fc8b9aa8c816294fb38e5
Romain22220 <romain.tacon@etudiant.univ-brest.fr>
2025-03-02 21:08:40 +0100

    Initialisation de l'API core-api
    - Ajout du CoreController
    - Ajout des DTO Manager/Student/Formation/Authentification/AcademicYear/Ue
    - Ajout du StudentClient

--- src/main/java/com/coreapi/core/CoreApiApplication.java
+++ src/main/java/com/coreapi/core/CoreApiApplication.java
@@ -3,7 +3,7 @@ package com.coreapi.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableFeignClients
+
 @SpringBootApplication
 public class CoreApiApplication {
 
