eb51f6ab531836f5bd5437964c08ed258aa0d4e3
Romain22220 <romain.tacon@etudiant.univ-brest.fr>
2025-03-06 11:39:32 +0100

    Modifications dans le core :
    - Ajout des constructeurs de AcademicYearDTO et AuthentificationDTO

--- src/main/java/com/coreapi/dto/AcademicYearDTO.java
+++ src/main/java/com/coreapi/dto/AcademicYearDTO.java
@@ -1,5 +1,12 @@
 package com.coreapi.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AcademicYearDTO {
     private Long id;
     private String year;
--- src/main/java/com/coreapi/dto/AuthentificationDTO.java
+++ src/main/java/com/coreapi/dto/AuthentificationDTO.java
@@ -1,5 +1,12 @@
 package com.coreapi.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthentificationDTO {
     private String username;
     private String password;
